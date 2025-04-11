9f889836dc1cc6f53ae96ca644bd32731fe70396
Yoann12 <yoannsev@gmail.com>
2025-04-11 15:35:25 +0200

    new changes 3

--- train.py
+++ train.py
@@ -33,17 +33,19 @@ in the data or code.
 """
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Loading the Dataset
-    drug_df = pd.read_csv('data/drug200.csv')
+    drug_df = pd.read_csv("data/drug200.csv")
     drug_df = drug_df.sample(frac=1)
     print(drug_df.head())
 
-    X = drug_df.drop('Drug', axis=1)
-    y = drug_df['Drug']
+    X = drug_df.drop("Drug", axis=1)
+    y = drug_df["Drug"]
 
     # Train Test Split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=125)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=125
+    )
 
     # Machine Learning Pipelines
     cat_col = [1, 2, 3]
@@ -51,16 +53,16 @@ if __name__ == '__main__':
 
     transform = ColumnTransformer(
         [
-            ('encoder', OrdinalEncoder(), cat_col),
-            ('num_imputer', SimpleImputer(strategy='median'), num_col),
-            ('num_scaler', StandardScaler(), num_col)
+            ("encoder", OrdinalEncoder(), cat_col),
+            ("num_imputer", SimpleImputer(strategy="median"), num_col),
+            ("num_scaler", StandardScaler(), num_col),
         ]
     )
 
     pipe = Pipeline(
         steps=[
-            ('preprocessing', transform),
-            ('model', RandomForestClassifier(n_estimators=100, random_state=125))
+            ("preprocessing", transform),
+            ("model", RandomForestClassifier(n_estimators=100, random_state=125)),
         ]
     )
 
@@ -69,22 +71,24 @@ if __name__ == '__main__':
     # Model Evaluation
     prediction = pipe.predict(X_test)
     accuracy = accuracy_score(y_test, prediction)
-    f1 = f1_score(y_test, prediction, average='macro')
+    f1 = f1_score(y_test, prediction, average="macro")
 
-    print(f'Accuracy: {accuracy:.2f}%, F1: {f1:.2f}')  # Our model has performed exceptionally well.
+    print(
+        f"Accuracy: {accuracy:.2f}%, F1: {f1:.2f}"
+    )  # Our model has performed exceptionally well.
 
     # Create the metrics file and save it in the Results folder.
-    with open('results/metrics.txt', 'w') as outfile:
-        outfile.write(f'Accuracy: {accuracy:.2f}, F1: {f1:.2f}\n')
+    with open("results/metrics.txt", "w") as outfile:
+        outfile.write(f"Accuracy: {accuracy:.2f}, F1: {f1:.2f}\n")
 
     # We will then create the confusion matrix and save the image file into the Results folder.
     cm = confusion_matrix(y_test, prediction, labels=pipe.classes_)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
     disp.plot()
-    plt.savefig('results/model_results.png')
+    plt.savefig("results/model_results.png")
 
     # Saving the Model
-    sio.dump(pipe, 'model/drug_pipeline.skops')
+    sio.dump(pipe, "model/drug_pipeline.skops")
 
     # You can just load the entire pipeline, and it will work out of the box without processing
     # your data or making edits to the code.
